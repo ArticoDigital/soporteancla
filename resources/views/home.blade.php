510423e003c707555a08730c0d7a7d382f91ba5b
juan2ramos <juan2ramos@gmail.com>
2018-09-24 20:10:29 -0500

    signin

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.layout')
+
+@section('content')
+
+        <form action="" class="Login row justify-center middle-items">
+            <div class="Login-container">
+                <h1 class="Login-h1">INICIAR SESIÓN</h1>
+                <input type="text" placeholder="Email">
+                <input type="password" placeholder="Contraseña">
+                <div class="row justify-end">
+                    <button type="submit">INGRESAR</button>
+                </div>
+            </div>
+        </form>
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+
+    <meta name="description" content="">
+    <meta name="author" content="juan2ramos">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title') </title>
+    <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+
+</head>
+<body data-site="{{env('APP_URL')}}" id="body">
+<header class="Header row align-end">
+
+    <div class="container row justify-between middle-items">
+        <figure class="Header-logo">
+            <img class="Header-logoImage" src="{{asset('images/logo_ancla_blanco.png')}}" alt="">
+        </figure>
+        <nav class="Header-nav">
+
+        </nav>
+    </div>
+
+</header>
+
+<main class="Main container">
+    @yield('content')
+</main>
+<footer class="Footer">
+    Cajas Fuertes Ancla S.A.S. Nit 860067604-7. Todos los derechos reservados
+</footer>
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
