70a0c7f0d3e75d686be5edb4f81ed2b91c657476
drqo <danielrqo@gmail.com>
2018-09-27 16:34:39 -0500

    fixcreateticket

--- resources/views/create-ticket.blade.php
+++ resources/views/create-ticket.blade.php
@@ -18,26 +18,26 @@
             <h1>¿Cómo puedo ayudarle?</h1>
             <div class="row">
                 <div class="col-8 p-r-20">
-                    <input type="text" name="name" placeholder="Nombre">
-                    <input type="text" name="company" placeholder="Empresa">
-                    <input type="text" name="cellphone" placeholder="Celular">
-                    <input type="email" name="email" placeholder="E-mail">
-                    <input type="text" name="subject" placeholder="Asunto">
+                    <input type="text" name="name" placeholder="Nombre" required  maxlength="100" value="{{old('name')}}">
+                    <input type="text" name="company" placeholder="Empresa" required  maxlength="100" value="{{old('company')}}">
+                    <input type="text" name="cellphone" placeholder="Celular" required  maxlength="20" value="{{old('cellphone')}}">
+                    <input type="email" name="email" placeholder="E-mail" required value="{{old('email')}}">
+                    <input type="text" name="subject" placeholder="Asunto" required  maxlength="200" value="{{old('subject')}}">
                 </div>
                 <div  class="col-8 p-l-20">
-                    <input type="text" name="identification" placeholder="Cédula o NIT">
-                    <input type="text" name="sell_point" placeholder="Punto de venta">
-                    <input type="text" name="operation_center" placeholder="Centro de operación">
+                    <input type="text" name="identification" placeholder="Cédula o NIT" required  maxlength="50" value="{{old('identification')}}">
+                    <input type="text" name="sell_point" placeholder="Punto de venta" required  maxlength="100" value="{{old('sell_point')}}">
+                    <input type="text" name="operation_center" placeholder="Centro de operación" required maxlength="150" value="{{old('operation_center')}}">
                     <label class="m-b-16" for="service_category">Categoría de servicio</label>
-                    <select name="service_category_id" id="service_category_id">
-                        <option value="0">Seleccione</option>
+                    <select name="service_category_id" id="service_category_id" required>
+                        <option value="">Seleccione</option>
                         @foreach($categories as $category)
                             <option value="{{$category->id}}">{{$category->name}}</option>
                         @endforeach
                     </select>
 
                 </div>
-                <textarea  name="request" id="" cols="30" rows="10" placeholder="Escribe tu solicitud"></textarea>
+                <textarea required  name="request" id="" cols="30" rows="10" placeholder="Escribe tu solicitud">{!! old('request') !!}</textarea>
             </div>
             <button class="m-t-16">Enviar Ticket</button>
         </div>
