5cc442cecb7fc42a81bbce70f00745a67b78c7e2
drqo <danielrqo@gmail.com>
2018-10-07 11:31:24 -0500

    profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,21 +1,43 @@
 @extends('layouts.layout')
 
 @section('content')
+@if ($errors->any())
+    <div class="alert-error">
+        <ul class="Error-ul is-list-less">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+
+        <span class="Error-close"><i class="far fa-times-circle"></i></span>
+    </div>
+@endif
+@if (\Session::has('messageok'))
+    <div class="alert-success">
+        <ul>
+            <li>{!! \Session::get('messageok') !!}</li>
+        </ul>
+    </div>
+@endif
+
     <h2 class="m-t-40">Mi perfil</h2>
     <div class="User">
+      <form method="post" action="{{route('profileUpdate')}}"  class="row  justify-between">
+          @csrf
         <div class="row  justify-between">
             <div class="col-8 p-r-20">
-                <input type="text" name="name" placeholder="Nombre" value="Juan Ramos">
-                <input type="text" name="identification" placeholder="Cédula" value="80921243214">
+                <input type="text" name="name" placeholder="Nombre" value="{{$user->name}}">
+                <input type="text" name="identification" placeholder="Cédula" value="{{$user->identification}}">
 
             </div>
             <div class="col-8 p-l-20">
-                <input type="email" name="email" placeholder="E-Mail" value="juan2ramos@gmail.com">
-                <input type="password" name="Contraseña" placeholder="Contraseña">
+                <input type="email" name="email" placeholder="E-Mail" value="{{$user->email}}">
+                <input type="password" name="password" placeholder="Contraseña">
             </div>
             <div class="col-8 m-t-20">
                 <button type="submit">Actualizar</button>
             </div>
         </div>
+        </form>
     </div>
 @endsection
