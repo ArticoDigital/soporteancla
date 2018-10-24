706866eee052897b8e6c5efc9895094b2c4b2cf2
Juan Ramos <juan2ramos@gmail.com>
2018-10-23 21:10:50 -0500

    error login, banner, arrow

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,35 +2,42 @@ import flatpickr from "flatpickr";
 import {Spanish} from "flatpickr/dist/l10n/es"
 import subcategories from './subcategories'
 import {Delete} from "./Delete";
+import animateScrollTo from 'animated-scroll-to';
 
 
 flatpickr(".dates", {
-    altInput: true,
-    "locale": "es",
-    mode: "range",
-    dateFormat: "Y-m-d",
+  altInput: true,
+  "locale": "es",
+  mode: "range",
+  dateFormat: "Y-m-d",
 });
 
 const errorClose = document.querySelector('.Error-close');
 if (errorClose) {
-    errorClose.addEventListener('click', function () {
-        document.querySelector('.alert-error').remove();
-    });
+  errorClose.addEventListener('click', function () {
+    document.querySelector('.alert-error').remove();
+  });
 }
 
 subcategories();
 
 const deleteElement = document.querySelectorAll('.delete');
 if (deleteElement) {
-    deleteElement.forEach(function (el) {
-        el.addEventListener('click', function (e) {
-            e.preventDefault();
-            Delete({
-                title: 'Estas seguro de eliminar?',
-                text: 'Recuerda que no podrás volver a recuperar la info',
-                formId: el
-            });
-        });
-    })
+  deleteElement.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+      e.preventDefault();
+      Delete({
+        title: 'Estas seguro de eliminar?',
+        text: 'Recuerda que no podrás volver a recuperar la info',
+        formId: el
+      });
+    });
+  })
+}
+const arrow = document.querySelector('#arrowToScroll');
+if (arrow) {
+  arrow.addEventListener('click', function () {
+    animateScrollTo(document.querySelector('.target'));
+  })
 }
 
