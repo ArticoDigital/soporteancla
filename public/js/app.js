47ac29410ffaa1fc87cdda61d8c5135810277412
juan2ramos <juan2ramos@gmail.com>
2019-07-18 18:52:17 -0500

    finish

--- public/js/app.js
+++ public/js/app.js
@@ -2543,6 +2543,7 @@ var subCategoriesEl = document.querySelector('#service_subcategory'),
     categoriesEl = document.querySelector('#service_category_id'),
     type_category = document.querySelector('#type_category'),
     spreadsheets = document.querySelector('#spreadsheets'),
+    otherType = document.querySelector('#otherType'),
     album = document.querySelector('#album');
 var categoriesJson = {};
 
@@ -2566,10 +2567,12 @@ function changeSubcategory() {
     if (parseInt(subCategoryId) === 6) {
         spreadsheets.classList.remove('is-hidden');
         album.classList.remove('is-hidden');
+        type_category.classList.remove('is-hidden');
+        otherType.classList.remove('is-hidden');
     } else {
-
         spreadsheets.classList.add('is-hidden');
         album.classList.add('is-hidden');
+        otherType.classList.add('is-hidden');
     }
 }
 
@@ -2592,13 +2595,6 @@ function change() {
             }
             subCategoriesEl.add(option);
         });
-
-        if (parseInt(categoryId) === 1) {
-
-            type_category.classList.remove('is-hidden');
-        } else {
-            type_category.classList.add('is-hidden');
-        }
     } else {
         subCategoriesEl.disabled = true;
     }
