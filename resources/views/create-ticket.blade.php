1446f486cdd2725a07ae27b5cec6013650e8a206
juan2ramos <juan2ramos@gmail.com>
2019-07-18 18:57:19 -0500

    finish

--- resources/views/create-ticket.blade.php
+++ resources/views/create-ticket.blade.php
@@ -46,6 +46,11 @@
                     <input id="otherForm" class="{{old('city_id') == 1124?'':'is-hidden'}}" type="text" name="city_text"
                            placeholder="¿Cuál?" value="{{old('city_text')}}">
 
+                    <select class="m-t-16 is-hidden"  id="spreadsheets"  name="spreadsheets"  >
+                        <option value="">Solicitud de planillas ?</option>
+                        <option {{old('spreadsheets') == 'si' ? 'selected':''}} value="si">Si</option>
+                        <option {{old('spreadsheets') == 'no' ? 'selected':''}} value="no">No</option>
+                    </select>
                 </div>
                 <div class="col-16 col-m-8 col-l-8  p-l-20 p-r-20">
                     <input type="text" name="name" placeholder="Nombre" required maxlength="100"
@@ -83,11 +88,6 @@
                         <option {{old('album') == 'no' ? 'selected':''}} value="no">No</option>
                     </select>
 
-                    <select class="m-t-16 is-hidden"  id="spreadsheets"  name="spreadsheets"  >
-                        <option value="">Solicitud de planillas ?</option>
-                        <option {{old('spreadsheets') == 'si' ? 'selected':''}} value="si">Si</option>
-                        <option {{old('spreadsheets') == 'no' ? 'selected':''}} value="no">No</option>
-                    </select>
 
                     <input type="text" value="{{old('otherType')}}" placeholder="Otro" name="otherType" id="otherType" class="is-hidden">
 
