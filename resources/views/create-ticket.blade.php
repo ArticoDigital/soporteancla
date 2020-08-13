def1e7b94062a9d69dec68360fcdcbccc76c9c60
Fer221075 <67390145+Fer221075@users.noreply.github.com>
2020-08-13 16:55:53 -0500

    Update create-ticket.blade.php

--- resources/views/create-ticket.blade.php
+++ resources/views/create-ticket.blade.php
@@ -133,7 +133,7 @@
             </div>
             <button class="m-t-16">Enviar Ticket</button>
                     </div>
-        <div class="m-t-10">
+        <div class="m-t-5">
         
         <p> Cajas Fuertes Ancla actuará como un medio facilitador en temas de conciliación entre clientes y transportadoras de valores, sin que éste sea su obligación. Somos un ente colaborador y mediador.</p>
 
